5859af0e0ac1e5233b927cdec020d4ab714c8c71
ohtaeg <otk1090@naver.com>
2019-12-24 16:09:00 +0900

    solution (programmers level2) : 기능 개발 문제 풀이

---
+++ src/main/java/programmers/level2/functiondevelop/Solution.java
@@ -0,0 +1,8 @@
+package programmers.level2.functiondevelop;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
