0443d36a34fb54cb8ec25c9508858421021fce0c
ohtaeg <otk1090@naver.com>
2019-12-26 18:40:39 +0900

    docs(update readme.md) : level2 프린터 항목 추가

---
+++ src/main/java/programmers/level2/printer/Solution.java
@@ -0,0 +1,17 @@
+package programmers.level2.printer;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+
+    public int print(int[] priorities, int location) {
+        PriorityQueue printer = new PriorityQueue<>();
+
+        return 0;
+
+    }
+}
