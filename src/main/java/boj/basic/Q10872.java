eb47fc4781f8c9997d688e3743c54cc68a150d61
ohtaeg <otk1090@naver.com>
2020-02-10 17:50:17 +0900

    solution (boj) : 팩토리얼 문제 풀이

---
+++ src/main/java/boj/basic/Q10872.java
@@ -0,0 +1,21 @@
+package boj.basic;
+
+import java.io.*;
+
+public class Q10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        long sum = 1;
+        for (int i=1; i<=n; i++) {
+            sum *= i;
+        }
+
+        bw.write(sum + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
