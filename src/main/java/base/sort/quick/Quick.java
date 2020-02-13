2366b054084ba8cd3317c4871278056f14a2162a
ohtaeg <otk1090@naver.com>
2020-02-13 21:33:34 +0900

    refactor (quick sort) : 중위법을 이용하도록 수정

--- src/main/java/base/sort/quick/Quick.java
+++ src/main/java/base/sort/quick/Quick.java
@@ -1,6 +1,9 @@
 package base.sort.quick;
 
+import java.util.Random;
+
 public class Quick {
+    private static final Random rand = new Random();
     public static void main(String[] args) {
         int[] arr = new int[]{29, 14, 10, 37, 13, 10, 1};
         Quick.sort(arr, 0, arr.length - 1);
@@ -11,22 +14,35 @@ public class Quick {
         if (begin < end) {
             int pivot = partition(arr, begin, end);
             sort(arr, begin, pivot - 1);
-            sort(arr, pivot + 1, end);
+            sort(arr, pivot, end);
         }
     }
 
     private static int partition(final int[] arr, final int begin, final int end) {
-        int pivotValue = arr[end];
-        int left = begin - 1;
+        int pivotValue = arr[(begin + end) / 2]; // median
+        int left = begin;
+        int right = end;
 
-        for (int i = begin; i < end; i++) {
-            if (arr[i] < pivotValue) { // pivot 값보다 값이 작으면
+        // 두 인덱스가 지나칠때까지 반복
+        while (left < right) {
+
+            // 전반부쪽에서 피벗보다 작으면 무시하고 인덱스를 오른쪽으로 ->
+            while (arr[left] < pivotValue) {
                 left++;
-                swap(arr, left, i);
+            }
+
+            // 후반부쪽에서 피벗보다 크면 무시하고 인덱스를 왼쪽으로 <-
+            while (pivotValue < arr[right]) {
+                right--;
+            }
+
+            // 두 인덱스가 지나치지 않았으면 값을 바꾼 후 인덱스 이동
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
             }
         }
-        left++;
-        swap(arr, left, end);
         return left;
     }
 
