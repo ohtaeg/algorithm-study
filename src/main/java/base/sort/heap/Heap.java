48c5c919d9cf1f9017a84a74c87598f552c90c7e
ohtaeg <otk1090@naver.com>
2020-02-17 23:06:52 +0900

    base (heap sort) : 힙 정렬 non recursive 추가

--- src/main/java/base/sort/heap/Heap.java
+++ src/main/java/base/sort/heap/Heap.java
@@ -3,28 +3,27 @@ package base.sort.heap;
 public class Heap {
     public static void main(String[] args) {
         int[] arr = { 23, 10, 60, 55, 4, 22, 2 };
-        int[] arr2 = { 2, 17, 8, 4, 65, 21, 7, 30 };
         Heap.sort(arr);
         print(arr);
 
-        Heap.sort(arr2);
+        System.out.println("== non recursive ===");
+
+        int[] arr2 = { 23, 10, 60, 55, 4, 22, 2 };
+        Heap.nonRecursiveSort(arr2);
         print(arr2);
     }
 
     private static void sort(final int[] arr) {
-        buildMaxHeap(arr);
+        int heapSize = arr.length;
+        // 마지막 노드의 부모 노드 (n/2)에서부터 시작, root(0)까지 반복해서 max heap 을 만든다.
+        for (int i = heapSize / 2; i >= 0; i--) {
+            heapify(arr, heapSize, i);
+        }
+
         for (int i = arr.length - 1; i > 0; i--) {
             // 마지막 node부터 root값을 바꿔나간다.
             swap(arr, 0, i);
             heapify(arr, i, 0);
-        }
-    }
-
-    // 마지막 노드의 부모 노드 (n/2)에서부터 시작, root(0)까지 반복해서 max heap 을 만든다.
-    private static void buildMaxHeap(final int[] arr) {
-        int heapSize = arr.length;
-        for (int i = heapSize / 2; i >= 0; i--) {
-            heapify(arr, heapSize, i);
         }
     }
 
@@ -52,6 +51,48 @@ public class Heap {
 
         swap(arr, parentNodeIndex, maxChildIndex);
         heapify(arr, heapSize, maxChildIndex);
+    }
+
+    /**
+     * non recursive
+     */
+    private static void nonRecursiveSort(final int[] arr) {
+        int heapSize = arr.length;
+        // 마지막 노드의 부모 노드 (n/2)에서부터 시작, root(0)까지 반복해서 max heap 을 만든다.
+        for (int i = heapSize / 2; i >= 0; i--) {
+            nonRecursiveHeapify(arr, heapSize, i);
+        }
+
+        for (int i = heapSize - 1; i > 0; i--) {
+            // 마지막 node부터 root값을 바꿔나간다.
+            swap(arr, 0, i);
+            nonRecursiveHeapify(arr, i, 0);
+        }
+    }
+
+    private static void nonRecursiveHeapify(final int[] arr, final int heapSize, final int parentNodeIndex) {
+        int leftChildIndex = parentNodeIndex * 2 + 1; // root를 0번부터 시작하기 때문에 + 1
+        int rightChildIndex = parentNodeIndex * 2 + 2; // root를 0번부터 시작하기 때문에 + 2
+        int parent = parentNodeIndex;
+        int maxChildIndex;
+
+        while (leftChildIndex < heapSize && rightChildIndex < heapSize) {
+            if (arr[leftChildIndex] > arr[rightChildIndex]) {
+                maxChildIndex = leftChildIndex;
+            }  else {
+                maxChildIndex = rightChildIndex;
+            }
+
+            // 부모 노드값이 자식보다 크면 연산 중지
+            if (arr[parent] >= arr[maxChildIndex]) {
+                return;
+            }
+
+            swap(arr, parent, maxChildIndex);
+            parent = maxChildIndex;
+            leftChildIndex = parent * 2 + 1;
+            rightChildIndex = parent * 2 + 2;
+        }
     }
 
     private static void swap(final int[] arr, final int i, final int j) {
