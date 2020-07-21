99548ab411c7852f760bf4f003d3a361fbf69020
ohtaeg <otk1090@naver.com>
2020-07-21 15:04:33 +0900

    (refactor) : 9012 괄호 문제 잡다한 비교연산 제거 리팩토링

--- src/main/java/boj/stack/Q9012.java
+++ src/main/java/boj/stack/Q9012.java
@@ -1,83 +1,55 @@
 package boj.stack;
 
 import java.io.*;
+import java.util.Stack;
 
 public class Q9012 {
-    private static final BufferedReader BUFFERED_READER = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter BUFFERED_WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final String OPEN_BRACKET = "(";
-    private static final String CLOSE_BRACKET = ")";
     private static final String YES = "YES";
     private static final String NO = "NO";
+    private static final char RIGHT_BRACKET = ')';
     private static final int ZERO = 0;
 
-    public static void main(String[] args) throws IOException {
-        StringBuilder result = new StringBuilder();
-        int count = insertCount();
-        String source;
-        for (int i = ZERO; i < count; i++) {
-            source = insertExample();
-
-            // 1. 길이가 짝수가 아니라면
-            if (isNotEvenLength(source)) {
-                result.append(formatAnswer(NO));
-                continue;
+    public static void main(String[] args) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int count = Integer.parseInt(bufferedReader.readLine());
+            while (count-- > ZERO) {
+                String source = bufferedReader.readLine();
+                bufferedWriter.write(validParenthesis(source));
+                bufferedWriter.newLine();
             }
-
-            // 2. ) 로 시작하거나 ( 로 끝난다면
-            if (isNotCorrectBracketExpression(source)) {
-                result.append(formatAnswer(NO));
-                continue;
-            }
-
-            // 3. stack이 남아있다면
-            result.append(formatAnswer(checkVPS(source)));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        BUFFERED_WRITER.write(result.toString());
-
-        BUFFERED_READER.close();
-        BUFFERED_WRITER.flush();
-        BUFFERED_WRITER.close();
     }
 
-    private static int insertCount() throws IOException {
-        final String inputData = BUFFERED_READER.readLine();
-        return Integer.valueOf(inputData);
-    }
+    private static String validParenthesis(final String source) {
+        Stack<Character> stack = new Stack<>();
 
-    private static String insertExample() throws IOException {
-        return BUFFERED_READER.readLine();
-    }
-
-    private static boolean isNotEvenLength(final String source) {
-        return source.length() % 2 != ZERO;
-    }
-
-    private static String formatAnswer(final String answer) {
-        return answer + System.lineSeparator();
-    }
-
-    private static boolean isNotCorrectBracketExpression(final String source) {
-        return source.startsWith(CLOSE_BRACKET) || source.endsWith(OPEN_BRACKET);
-    }
-
-    private static String checkVPS(final String source) {
-        int count = ZERO;
         for (int i = ZERO; i < source.length(); i++) {
-            String bracket = String.valueOf(source.charAt(i));
-            if (OPEN_BRACKET.equals(bracket)) {
-                count++;
-            }
-
-            if (CLOSE_BRACKET.equals(bracket)) {
-                if (count == ZERO) {
+            final char bracket = source.charAt(i);
+            if (isRightBracket(bracket)) {
+                if (stack.isEmpty()) {
                     return NO;
                 }
-                count--;
+                stack.pop();
+            } else {
+                stack.push(bracket);
             }
         }
 
-        return count == ZERO ? YES : NO;
+        return isVPS(stack);
+    }
+
+    private static boolean isRightBracket(final char bracket) {
+        return RIGHT_BRACKET == bracket;
+    }
+
+    private static String isVPS(final Stack<Character> stack) {
+        if (stack.isEmpty()) {
+            return YES;
+        } else {
+            return NO;
+        }
     }
 }
