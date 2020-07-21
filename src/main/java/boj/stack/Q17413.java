b43ab64aeb05350ac860d5fb12cac0de098d6bc0
ohtaeg <otk1090@naver.com>
2020-07-21 17:51:10 +0900

    (refactor) : 단어뒤집기 2 다시 풀기

--- src/main/java/boj/stack/Q17413.java
+++ src/main/java/boj/stack/Q17413.java
@@ -1,51 +1,60 @@
 package boj.stack;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 
 /**
  * Reverse word 2
  */
 public class Q17413 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<String> wordStack = new Stack<>();
-        String source = br.readLine();
-        boolean isTag = false;
-        br.close();
+    private static final String LEFT_BRACKET = "<";
+    private static final String RIGHT_BRACKET = ">";
 
-        String word;
-        for (char ch : source.toCharArray()) {
-            word = String.valueOf(ch);
-            if (word.equals("<")) {
-                print(wordStack);
-                System.out.print(word);
-                isTag = true;
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        boolean isOpeningTag = false;
 
-            } else if (word.equals(">")) {
-                isTag = false;
-                System.out.print(word);
-            // 단어, 공백인데 태그가 열려있다면
-            } else if (isTag) {
-                System.out.print(word);
-            // 단어, 공백인데 태그가 닫혀 있다면
-            } else {
-                if (word.equals(" ")) {
-                    print(wordStack);
-                    System.out.print(word);
-                } else {
-                    wordStack.push(word);
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            String source = bufferedReader.readLine();
+            for (int i = 0; i < source.length(); i++) {
+                String word = source.substring(i, i + 1);
+                switch (word) {
+                    case LEFT_BRACKET :
+                        reverse(result, stack);
+                        isOpeningTag = true;
+                        result.append(word);
+                        break;
+                    case RIGHT_BRACKET :
+                        isOpeningTag = false;
+                        result.append(word);
+                        break;
+                    default :
+                        if (isOpeningTag) {
+                            result.append(word);
+                        } else {
+                            if (word.equals(" ")) {
+                                reverse(result, stack);
+                                result.append(word);
+                            } else {
+                                stack.push(word);
+                            }
+                        }
                 }
             }
+
+            reverse(result, stack);
+            bufferedWriter.write(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        print(wordStack);
     }
 
-    private static void print(final Stack<String> stack) {
-        while(!stack.isEmpty()) {
-            System.out.print(stack.pop());
+    private static void reverse(final StringBuilder result, final Stack<String> stack) {
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
         }
     }
 }
